3a7aa9911dfb7181236193db4c7f502efe7e11f6
Chris W Jones <chris@christopherjones.us>
2022-04-28 11:32:24 -0400

    Adds ability to create URLs with shortcodes

--- models/shortened_link.go
+++ models/shortened_link.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/gobuffalo/pop/v6"
 	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/gofrs/uuid"
+	"github.com/lithammer/shortuuid/v4"
 )
 
 // ShortenedLink is used by pop to map your shortened_links database table to your go code.
@@ -36,7 +38,10 @@ func (s ShortenedLinks) String() string {
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
 func (s *ShortenedLink) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
+	return validate.Validate(
+		&validators.URLIsPresent{Field: s.URL, Name: "URL"},
+		&validators.StringIsPresent{Field: s.ShortCode, Name: "ShortCode"},
+	), nil
 }
 
 // ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
@@ -48,5 +53,14 @@ func (s *ShortenedLink) ValidateCreate(tx *pop.Connection) (*validate.Errors, er
 // ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
 // This method is not required and may be deleted.
 func (s *ShortenedLink) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	s.Validate(tx)
+
 	return validate.NewErrors(), nil
+}
+
+func (s *ShortenedLink) BeforeValidate(tx *pop.Connection) error {
+	if s.ShortCode == "" {
+		s.ShortCode = shortuuid.New()
+	}
+	return nil
 }
