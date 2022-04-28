0936a43249cd05fa20225519566c3d57f96378a7
Chris W Jones <chris@christopherjones.us>
2022-04-28 11:14:13 -0400

    Creates initial shortened links resource

--- actions/app.go
+++ actions/app.go
@@ -63,8 +63,7 @@ func App() *buffalo.App {
 		// Setup and use translations:
 		app.Use(translations())
 
-		app.GET("/", HomeHandler)
-
+		app.Resource("/shortened_links", ShortenedLinksResource{})
 		app.ServeFiles("/", http.FS(public.FS())) // serve files from the public directory
 	}
 
